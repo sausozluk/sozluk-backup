6a153352292ca686883d367179c547ed5c8ac75f
Eray Arslan <relfishere@gmail.com>
2017-07-18 20:22:38 +0300

    backup app fixed

--- app.js
+++ app.js
@@ -5,23 +5,24 @@ var dotenv = require('dotenv').config();
 var env = process.env['SOZLUK_ENV'] || 'local';
 var config = require(__dirname + '/confs/' + env);
 
-var files = fs.readdirSync(__dirname + '/backups').filter(function (file) {
-  return file.indexOf('.tar') > 0;
-}).sort(function (a, b) {
-  var x = a.substr(0, a.length - 4);
-  var y = b.substr(0, b.length - 4);
-  return parseInt(x) < parseInt(y);
-});
+var doClean = function () {
+  var files = fs.readdirSync(__dirname + '/backups')
+    .filter(function (file) {
+      return file.indexOf('.tar') > 0;
+    }).sort(function (a, b) {
+      var x = a.substr(0, a.length - 4);
+      var y = b.substr(0, b.length - 4);
+      return parseInt(x) < parseInt(y);
+    });
 
-for (var i = 2; i < files.length; i++) {
-  var name = files[i];
-  fs.unlinkSync(__dirname + '/backups/' + name);
-}
+  for (var i = 2; i < files.length; i++) {
+    var name = files[i];
+    fs.unlinkSync(__dirname + '/backups/' + name);
+    console.log('[SOZLUK-CLEAN] Cleaned', name);
+  }
+};
 
 var doBackup = function () {
-  var time = new Date();
-  console.log('[SOZLUK-BACKUP] Started', 'at', time);
-
   backup({
     uri: config['mongo_uri'],
     root: __dirname + '/backups',
@@ -31,13 +32,16 @@ var doBackup = function () {
       if (err) {
         console.error('[ERR]', err.message);
       } else {
-        console.log('[SOZLUK-BACKUP] End    ', 'at', new Date());
+        console.log('[SOZLUK-BACKUP] Done at', new Date());
       }
     }
   });
 };
 
-var job = new cronjob('00 00 7-23/2 * * *', doBackup, function () {
+var job = new cronjob('00 00 7-23/2 * * *', function () {
+  doClean();
+  doBackup();
+}, function () {
 }, true, 'Europe/Istanbul');
 
 console.log('[APP]', 'running with', env);
